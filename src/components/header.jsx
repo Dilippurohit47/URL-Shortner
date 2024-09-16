909be2f8c847abdd7b7efa3efc2646acb68e802c
Dilippurohit47 <dilippurohit204@gmail.com>
2024-09-16 21:23:07 +0530

    header

---
+++ src/components/header.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import LOGO from "../assets/logo.png";
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Link2Icon, Link2OffIcon, LogOut } from "lucide-react";
+
+const Header = () => {
+  const navigate = useNavigate();
+  const user = true;
+  return (
+    <nav className="py-4 px-10 flex justify-between items-center">
+      <Link to="/">
+        <img src={LOGO} className="h-16" alt="logo" />
+      </Link>
+      <div>
+        {!user ? (
+          <Button onClick={() => navigate("/auth")}>Login</Button>
+        ) : (
+          <DropdownMenu>
+            <DropdownMenuTrigger className="w-10 rounded-full outline-none overflow-hidden">
+              <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>Dilip Purohit</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <Link2Icon className="mr-2 h-4 w-4 text-blue-500 " />
+                My Links
+              </DropdownMenuItem>
+              <DropdownMenuItem className="text-red-500">
+                <LogOut className="mr-2 h-4 w-4" />
+                <span>Logout</span>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -1,9 +1,11 @@
+import Header from '@/components/header'
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 
 const AppLayout = () => {
   return (
     <div>
+      <Header />
       <main className='min-h-screen container'>
       <Outlet/>
       </main>
