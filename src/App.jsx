49a72ae619cac51978d34b50ba50adbea1bc7a1e
Dilippurohit47 <dilippurohit204@gmail.com>
2024-09-16 20:38:28 +0530

    route setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,46 @@
 import React from 'react'
 import { Button } from './components/ui/button'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import AppLayout from './layouts/app-layout'
+import HomePage from './pages/HomePage'
+import Dashboard from './pages/dashboard'
+import Auth from './pages/auth'
+import Link from './pages/link'
+import RedirectLink from './pages/redirectLink'
 
+
+const router  = createBrowserRouter([
+
+  {
+    element:<AppLayout/>,
+    children:[
+      {
+        path: '/',
+        element: <HomePage/>
+      },
+      {
+        path: '/dashboard',
+        element: <Dashboard/>
+      },
+      {
+        path: '/auth',
+        element: <Auth/>
+      },
+      {
+        path: '/link/:id',
+        element: <Link/>
+      },
+      {
+        path: '/:id',
+        element: <RedirectLink/>
+      },
+    ]
+  }
+
+])
 const App = () => {
   return (
-    <div>
-  <h1 className="text-3xl font-bold text-red-400 underline">
-  <Button>Click me</Button>
-    </h1>
-    </div>
+   <RouterProvider router={router} />
   )
 }
 
---
+++ src/layouts/app-layout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+const AppLayout = () => {
+  return (
+    <div>
+      <main className='min-h-screen container'>
+      <Outlet/>
+      </main>
+      <div className='p-10 text-center bg-gray-800 mt-10 '>
+        Made with ❤️ By Me
+      </div>
+    </div>
+  )
+}
+
+export default AppLayout
